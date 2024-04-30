fb1ed192676d5b8efbfcce3c6072b30259c8e4a0
KnoxDoNothing <ldiafye@gmail.com>
2024-04-30 09:54:40 +0800

    Add Analytics & modify SpeedInsighs

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,22 +3,19 @@ import "@/styles/globals.css";
 import { Toaster } from "react-hot-toast";
 import { CartProvider } from "use-shopping-cart";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from '@vercel/analytics/react';
 
 const stripeKey = process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY;
 
 export default function App({ Component, pageProps }) {
   return (
-    <SpeedInsights>
-      <CartProvider
-        stripe={stripeKey}
-        cartMode="checkout-session"
-        currency="USD"
-      >
-        <AppLayout>
-          <Component {...pageProps} />
-          <Toaster />
-        </AppLayout>
-      </CartProvider>
-    </SpeedInsights>
+    <CartProvider stripe={stripeKey} cartMode="checkout-session" currency="USD">
+      <AppLayout>
+        <Component {...pageProps} />
+        <Toaster />
+        <SpeedInsights />
+        <Analytics/>
+      </AppLayout>
+    </CartProvider>
   );
 }
